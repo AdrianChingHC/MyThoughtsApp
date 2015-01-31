8b165904e38cc320e3e98fa493a57cbba7819d8d
AdrianChingHC <achc92@gmail.com>
2015-01-31 10:07:44 +0800

    Test for invalid email addresses format

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -36,4 +36,12 @@ class UserTest < ActiveSupport::TestCase
   		assert @user.valid?, "#{v.inspect} should be valid"
   	end
   end
+
+  test "email validation should reject invalid addresses" do
+  	invalid_addresses = %w[user@mythoughtsapp,com user.first@mythoughtsapp. user@my_thoughtsapp.com user@my+thoughtsapp.com my_name-is_user.org]
+  	invalid_addresses.each do |i|
+  		@user.email = i
+  		assert_not @user.valid?, "#{i.inspect} should not be valid"
+  	end
+  end
 end
