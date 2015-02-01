ddb8a53ea4db1142ac2b4eb4eb7b0da159df5a04
AdrianChingHC <achc92@gmail.com>
2015-02-01 16:51:32 +0800

    Implement authorization for user update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -64,5 +65,12 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "You must log in to access this page."
+        redirect_to login_url
+      end
     end
 end
