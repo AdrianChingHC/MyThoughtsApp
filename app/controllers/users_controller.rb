59c3f8f8b93e953b256a3c6d39ee70213a8a43d8
AdrianChingHC <achc92@gmail.com>
2015-02-01 17:05:46 +0800

    Implement authorization between users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,6 +60,7 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
