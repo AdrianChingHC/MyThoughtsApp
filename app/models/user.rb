fa87bb3ccde823c0b3d602b81973e7f04eb4b089
AdrianChingHC <achc92@gmail.com>
2015-02-02 11:33:59 +0800

    Add destroy dependent to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
-  has_many :thoughts
+  has_many :thoughts, dependent: :destroy
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 255},
-                    format:     { with: VALID_EMAIL_REGEX },
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 8 }, allow_blank: true
