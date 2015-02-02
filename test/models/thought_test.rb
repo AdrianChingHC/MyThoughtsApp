5812ebb496bee7f9d1b8723457a9316daa1762d3
AdrianChingHC <achc92@gmail.com>
2015-02-02 09:45:05 +0800

    Test for validity of new thoughts

--- test/models/thought_test.rb
+++ test/models/thought_test.rb
@@ -4,4 +4,17 @@ class ThoughtTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:admin)
+    @thought = Thought.new(content: "This is a thought", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @thought.valid?
+  end
+
+  test "user id should be present" do
+    @thought.user_id = nil
+    assert_not @thought.valid?
+  end
 end
