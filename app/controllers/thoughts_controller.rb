0a2ab35c1102349498d3b9cd33ca38fa54a11cbf
AdrianChingHC <achc92@gmail.com>
2015-02-02 15:51:11 +0800

    Add before action to thoughts controller for authorized users

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -1,5 +1,6 @@
 class ThoughtsController < ApplicationController
 	include SessionsHelper
+	before_action :logged_in_user, only: [:create, :destroy]
 
 	def index
 		@thoughts = current_user.thoughts.paginate(page: params[:page])
