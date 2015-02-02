f37d2a3f6186bf5416e910455687481b378eb2c9
AdrianChingHC <achc92@gmail.com>
2015-02-02 09:22:28 +0800

    Fix code in user edit test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -22,18 +22,17 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
     log_in_as(@user)
-    get edit_user_path(@user)
     assert_redirected_to edit_user_path(@user)
     name  = "User Name"
     email = "user@mythoughtsapp.com"
-    patch user_path(@user), user: { name:  name,
+    patch user_path(@user), user: { name: name,
                                     email: email,
-                                    password:              "",
-                                    password_confirmation: "" }
+                                    password:              "password",
+                                    password_confirmation: "password" }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
-    assert_equal @user.name,  name
+    assert_equal @user.name, name
     assert_equal @user.email, email
   end
 end
