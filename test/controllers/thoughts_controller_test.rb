41c590f71f137426a6a6a06c03434b0a76c775cd
AdrianChingHC <achc92@gmail.com>
2015-02-02 15:38:20 +0800

    Test for redirection of create and destroy action if user not logged in

--- test/controllers/thoughts_controller_test.rb
+++ test/controllers/thoughts_controller_test.rb
@@ -4,4 +4,21 @@ class ThoughtsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @thought = thoughts(:first)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'thoughts.count' do
+      post :create, thought: { content: "Lorem ipsum" }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'thoughts.count' do
+      delete :destroy, id: @thought
+    end
+    assert_redirected_to login_url
+  end
 end
