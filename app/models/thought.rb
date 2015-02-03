56ee3fcebd99c1f47e84ac1acbffd03d4520c0fa
AdrianChingHC <achc92@gmail.com>
2015-02-03 15:01:06 +0800

    Hashtag links working

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,7 +1,5 @@
 class Thought < ActiveRecord::Base
 	include SimpleHashtag::Hashtaggable
-	include HashtagsHelper
-	include SessionsHelper
 	hashtaggable_attribute :content
   belongs_to :user
   validates :user_id, presence: true
@@ -9,6 +7,10 @@ class Thought < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
 
   def linkify
-  	linkify_hashtags(Thought.find(@thought.id))
-  end
+  	regex = SimpleHashtag::Hashtag::HASHTAG_REGEX
+			hashtagged_content = content.to_s.gsub(regex) do
+  			"<a href='/hashtags/#{$2}' class='hashtag'>#{$&}</a>"
+			end
+		hashtagged_content.html_safe
+	end
 end
